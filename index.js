b3ddedcb0b3c6796e4d1fdfd3241b71ca0a9a9ec
nahuel <nahuel.kp2010@hotmail.es>
2024-10-17 15:37:33 -0300

    Se creo socket.io para la comunicacion entre el front

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { Server } = require('socket.io');
+const { createServer } = require('node:http');
+const http = require('http');
 const cors = require('cors'); // Importar el paquete cors
-
 const connectDB = require('./db');
 const { initMqttClient } = require('./mqttService');
 
@@ -10,27 +12,47 @@ const app = express();
 const PORT = 5000;
 
 // Habilitar CORS
-app.use(cors({
-    origin: 'http://localhost:3000' // Cambia esto por el dominio de tu frontend
-}));
-// Conectar a la base de datos
-connectDB();
+// app.use(cors({
+//     origin: 'http://localhost:3000' // Cambia esto por el dominio de tu frontend
+// }));
 
-
-// Rutas para obtener los estados
-app.get('/estados', async (req, res) => {
-    try {
-        const estados = await Estados.find().sort({ fecha: -1 }).limit(1); // Obtener el último estado
-        res.json(estados[0]); // Retornar solo el último registro
-    } catch (error) {
-        console.error('Error al obtener los estados:', error);
-        res.status(500).json({ error: 'Error interno del servidor' });
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000", 
+        methods: ["GET", "POST"]
     }
 });
 
+io.on('connection', (socket) => {
+    
+    console.log("A user has connected!");
+    socket.on('message test', (message) => {
+        console.log("Mensaje recibido desde el front:", message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+});
+
+// Conectar a la base de datos
+connectDB();
+
+// Rutas para obtener los estados
+// app.get('/estados', async (req, res) => {
+//     try {
+//         const estados = await Estados.find().sort({ fecha: -1 }).limit(1); // Obtener el último estado
+//         res.json(estados[0]); // Retornar solo el último registro
+//     } catch (error) {
+//         console.error('Error al obtener los estados:', error);
+//         res.status(500).json({ error: 'Error interno del servidor' });
+//     }
+// });
+
 initMqttClient();
 // Iniciar el servidor
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
