62939b49613965654892919b6e72bc18847f5152
nicoonievas <nicolasnievas1@gmail.com>
2024-10-22 18:56:23 -0300

    esquema con fecha

--- estadosModel.js
+++ estadosModel.js
@@ -7,9 +7,8 @@ const estadosSchema = new mongoose.Schema({
     estado: { type: String, required: true },
     readTime: { type: String, required: true },
     ventilador: { type: Boolean, required: true },
-    nivelVida: { type: Number, required: true }
- 
-    // fecha: { type: Date, default: Date.now }  // Timestamp por defecto
+    nivelVida: { type: Number, required: true }, 
+    fecha: { type: Date, default: Date.now }  // Timestamp por defecto
 });
 
 const Estados = mongoose.model('estados', estadosSchema);
