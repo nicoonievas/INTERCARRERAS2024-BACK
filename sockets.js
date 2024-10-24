9bd481f9fbc6e68945426ef1d00d5ee25e8a8b67
nahuel <nahuel.kp2010@hotmail.es>
2024-10-24 11:39:01 -0300

    se agrego la funcion para recibir los datos que se envian desde el front por sockets

--- sockets.js
+++ sockets.js
@@ -1,35 +1,38 @@
-import { Server } from 'socket.io';
-import Estados from './estadosModel.js';
+import { Server as socketIO } from "socket.io";
 
 const initSocket = (server) => {
-    const io = new Server(server, {
-        cors: {
-            origin: "http://localhost:3000",
-            methods: ["GET", "POST"]
-        }
+  const io = new socketIO(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Cliente conectado:", socket.id);
+
+    socket.on("accion_mascota", (data) => {
+      console.log(`Acción de mascota recibida: ${data.action}`);
+      console.log(`Detalles: `, data);
+
+      switch (data.action) {
+        case "alimentar":
+          console.log("La mascota ha sido alimentada.");
+          break;
+        case "dormir":
+          console.log("La mascota está durmiendo.");
+          break;
+        case "curar":
+          console.log("La mascota ha sido curada.");
+          break;
+        default:
+          console.log("Acción desconocida:", data.action);
+      }
     });
 
-    io.on('connection', (socket) => {
-        console.log("A user has connected!");
-
-        const obtenerUltimoRegistro = async () => {
-            try {
-                const ultimoEstado = await Estados.findOne().sort({ _id: -1 }); // Obtener el último registro
-
-                console.log(ultimoEstado);
-
-                if (ultimoEstado) { 
-                    socket.emit('nuevas_estadisticas', ultimoEstado);
-                } else {
-                    console.log('No se encontró ningún estado');
-                }
-            } catch (error) {
-                console.error('Error al obtener el último registro:', error);
-            }
-        };
-
-        obtenerUltimoRegistro();
+    socket.on("disconnect", () => {
+      console.log("Cliente desconectado:", socket.id);
     });
+  });
 };
 
 export default initSocket;
